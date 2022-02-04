e70b346bab6d918292ec74b1b989d5c555443576
Misha Shatokhin <Shatokhingti@gmail.com>
2022-02-04 12:49:06 -0500

    completed step 4

---
+++ 15_Review_Day/exercise/PetElevator/CRM/Pet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetElevator.CRM
+{
+    class Pet
+    {
+        //Constructors
+        public Pet (string petName, string species)
+        {
+            PetName = petName;
+            Species = species;
+        }
+
+        //Properties
+        public string PetName { get; set; }
+        public string Species { get; set; }
+        public List<Pet> Vaccinations { get; set; } = new List<Pet>();
+
+        //Methods
+        public string ListVaccinations()
+        {
+            string petVacs = string.Join(",", Vaccinations);
+            
+            return petVacs;
+        }
+
+    }
+}
